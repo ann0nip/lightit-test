320951ffc7963714a9c7e6944d0c796ce7e7f40b
Juan Martin Gimenez <j.martin.utn@gmail.com>
2022-09-27 23:46:29 -0300

    SideBar buttons styles & behavior done

--- src/components/side-bar/side-bar.component.tsx
+++ src/components/side-bar/side-bar.component.tsx
@@ -1,17 +1,47 @@
+import cx from 'classnames';
+import React, { useState } from 'react';
+
 import Aberturas from '../../assets/icons/Aberturas.png'
 import Equipamiento from '../../assets/icons/Equipamiento.png'
 import Terminaciones from '../../assets/icons/Terminaciones.png'
 
+const MENU_ITEMS = [{
+    id: 0,
+    name: 'Aberturas',
+    icon: Aberturas
+}, {
+    id: 1,
+    name: 'Equipamiento',
+    icon: Equipamiento
+}, {
+    id: 2,
+    name: 'Terminaciones',
+    icon: Terminaciones
+}]
 
 const SideBar = () => {
+    const [categorySelected, setCategorySelected] = useState<number>()
+
+    const handleClick = (category: number) => {
+        setCategorySelected(category)
+
+    }
     return (
         <nav className='flex items-center'>
             <ul>
-                <li className='p-2 flex flex-col items-center hover:bg-gray-100'><img src={Aberturas} alt="aberturas button" /><span className='text-xs'>Aberturas</span></li>
-                <li className='p-2 flex flex-col items-center hover:bg-gray-100'><img src={Equipamiento} alt="equipamiento button" /><span className='text-xs'>Equipamiento</span></li>
-                <li className='p-2 flex flex-col items-center hover:bg-gray-100'><img src={Terminaciones} alt="terminaciones button" /><span className='text-xs'>Terminaciones</span></li>
-            </ul>
-        </nav>
+                {MENU_ITEMS.map((item) => (
+                    <li key={item.id}>
+                        <div onClick={() => handleClick(item.id)} className={cx('p-2 flex flex-col items-center hover:bg-[#F7F7F7]', {
+                            'box-active': categorySelected === item.id
+                        })}>
+                            <div className='top'></div>
+                            <img src={item.icon} alt={`${item.icon} button`} /><span className='text-xs'>{item.name}</span>
+                            <div className='bottom'></div>
+                        </div>
+                    </li>
+                ))}
+            </ul >
+        </nav >
     )
 }
 
